551bf6c04a8278219605f0c8e3a7fa0e4512ff24
VoidDev-0 <dev.avax@protonmail.com>
2023-04-16 01:04:27 +0200

    Revert "[ALPS04128327] sched/cpufreq: uclamp: add utilization clamping for RT tasks"
    This reverts commit 0d55dd59d1330d0f45fb1c57d814f2376b7615a4.
    
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -278,7 +278,8 @@ static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 		 * blocked RT utilization.
 		 */
 		max_boost = sg_cpu->iowait_boost_max;
-		max_boost = uclamp_util(sg_cpu->cpu, max_boost);
+		if (!sched_get_rt_rq_util(sg_cpu->cpu))
+			max_boost = uclamp_util(sg_cpu->cpu, max_boost);
 
 		if (sg_cpu->iowait_boost) {
 			sg_cpu->iowait_boost <<= 1;
